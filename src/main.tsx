9d96084068bb0cd237baee9cdebd66fa53cd8d54
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-12-26 10:14:48 +0100

    App

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './components/App'
+import App from './components'
 import './assets/styles/style.css'
 
 createRoot(document.getElementById('root')!).render(
