09dc2527aa7dffca99aff58e32cd2a6057f362d9
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-12-30 06:10:00 +0100

    Header/Sidebar

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from '../../pages/Home'
-import Header from '../../layout/Header'
-import SideBar from '../../layout/Sidebar'
+import Home from '../pages/Home'
+import Header from '../layout/Header'
+import SideBar from '../layout/Sidebar'
 
 function App() {
   return (
