d9bf379da4c46979a8b1b60474aa13ef2a0b6717
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-12-30 06:55:10 +0100

    Activity => recharts

---
+++ src/components/Activity/CustomToolTip.tsx
@@ -0,0 +1,13 @@
+const CustomToolTip: React.FC = ({ active, payload }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="tooltip">
+        <p>{payload[0].value + 'kg'}</p>
+        <p>{payload[1].value + 'Kcal'}</p>
+      </div>
+    )
+  }
+  return null
+}
+
+export default CustomToolTip
--- src/components/Activity/index.tsx
+++ src/components/Activity/index.tsx
@@ -1,5 +1,98 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { API } from '../../services'
+const { getUserActivity } = API
+
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import CustomToolTip from './CustomToolTip'
+
 const Activity: React.FC = () => {
-  return <div className="activity"></div>
+  const [state, setState] = useState([])
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchActivity = async (id?: string) => {
+      const activity = await getUserActivity(id)
+      setState(activity)
+    }
+    fetchActivity(id).catch(console.error)
+  }, [id])
+
+  return (
+    <div className="activity">
+      <h2 className="activity_title">Activité quotidienne</h2>
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={state}
+          barGap={8}
+          barCategoryGap={2}
+          barSize={7}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid vertical={false} strokeDasharray="3 3" />
+          <XAxis
+            dataKey="day"
+            tick={{ fill: '#9B9EAC' }}
+            tickLine={false}
+            stroke="#DEDEDE"
+            strokeWidth={2}
+            tickMargin={16}
+          />
+          <YAxis
+            yAxisId="kilogram"
+            orientation="right"
+            tickMargin={30}
+            tick={{ fill: '#9B9EAC' }}
+            tickLine={false}
+            axisLine={false}
+            domain={['dataMin-2', 'dataMax+1']}
+            tickCount={3}
+          />
+          <YAxis hide yAxisId="calories" />
+          <Tooltip
+            cursor={{ fill: 'rgba(196, 196, 196, 0.5)' }}
+            content={<CustomToolTip active={false} payload={[]} />}
+          />
+          <Bar
+            name="Poids (kg)"
+            dataKey="kilogram"
+            yAxisId="kilogram"
+            fill="#282D30"
+            radius={[3, 3, 0, 0]}
+          />
+          <Bar
+            name="Calories brûlées (kCal)"
+            dataKey="calories"
+            yAxisId="calories"
+            fill="#E60000"
+            radius={[3, 3, 0, 0]}
+          />
+          <Legend
+            verticalAlign="top"
+            align="right"
+            iconType="circle"
+            iconSize={10}
+            height={80}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default Activity
