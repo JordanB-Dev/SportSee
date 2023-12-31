f39b661ddc570ccaba0f203347bf2131a6d94b99
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-12-31 08:22:21 +0100

    UserPerf => recharts

--- src/components/UserPerf/index.tsx
+++ src/components/UserPerf/index.tsx
@@ -3,6 +3,14 @@ import { useParams } from 'react-router-dom'
 
 import { API } from '../../services'
 import { TypePerformance } from '../../Types'
+import {
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  RadarChart,
+  ResponsiveContainer,
+} from 'recharts'
 
 const { getUserPerformance } = API
 
@@ -19,7 +27,27 @@ const UserPerf: React.FC = () => {
     fetchPerformance(id).catch(console.error)
   }, [id])
 
-  return <div className="performance"></div>
+  return (
+    <div className="performance">
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChart outerRadius={90} data={perf!.reverse()}>
+          <PolarGrid radialLines={false} />
+          <PolarAngleAxis
+            dataKey="category"
+            tick={{ fill: 'white', fontSize: 12, fontWeight: '500' }}
+            offset={100}
+          />
+          <PolarRadiusAxis tickCount={6} tick={false} axisLine={false} />
+          <Radar
+            dataKey="value"
+            stroke="#FF0101"
+            fill="#FF0101"
+            fillOpacity={0.7}
+          />
+        </RadarChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default UserPerf
