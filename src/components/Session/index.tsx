0b7e2c47fe596bd5d685906234d102a403f6adad
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2023-12-31 07:49:46 +0100

    Session => recharts

--- src/components/Session/index.tsx
+++ src/components/Session/index.tsx
@@ -3,6 +3,16 @@ import { useParams } from 'react-router-dom'
 
 import { API } from '../../services'
 import { TypeSession } from '../../Types'
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import CustomToolTip from './CustomToolTip'
 
 const { getUserSession } = API
 
@@ -20,10 +30,51 @@ const Session: React.FC = () => {
 
   return (
     <div className="session">
-      {' '}
       <h3 className="session_title">
         Durée moyenne des <br /> sessions
       </h3>
+
+      <ResponsiveContainer width="98%" height="100%">
+        <LineChart data={state}>
+          <CartesianGrid fill="#FF0000" stroke="#FF0000" />
+
+          <XAxis
+            dataKey="day"
+            stroke="rgba(255,255,255,0.75)"
+            tick={{ fontSize: 12, fontWeight: 500 }}
+            axisLine={false}
+            tickLine={false}
+          />
+
+          <YAxis
+            dataKey="value"
+            hide
+            domain={['dataMin-10', 'dataMax+10']}
+            padding={{ top: 20 }}
+          />
+
+          <Line
+            type="natural"
+            dataKey="value"
+            stroke="url(#colorUv)"
+            strokeWidth={2}
+            dot={false}
+            activeDot={{ r: 3, strokeWidth: 2, stroke: 'white' }}
+          />
+
+          <defs>
+            <linearGradient id="colorUv" x1="0%" y1="0" x2="100%" y2="0">
+              <stop offset="0%" stopColor="rgba(255, 255, 255, 0.3)" />
+              <stop offset="20%" stopColor="rgba(255, 255, 255, 0.4)" />
+              <stop offset="40%" stopColor="rgba(255, 255, 255, 0.5)" />
+              <stop offset="60%" stopColor="rgba(255, 255, 255, 0.6)" />
+              <stop offset="100%" stopColor="rgba(255, 255, 255, 1)" />
+            </linearGradient>
+          </defs>
+
+          <Tooltip content={<CustomToolTip />} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
