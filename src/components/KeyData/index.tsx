a0d95181f1acb86e1c7a9af7519fbf9b20b69252
Jordan Bachelez <101954241+JordanB-Dev@users.noreply.github.com>
2024-01-03 05:14:50 +0100

    KeyData

--- src/components/KeyData/index.tsx
+++ src/components/KeyData/index.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { API } from '../../services'
+
+const { getKeyData } = API
 
 const KeyData: React.FC = () => {
   const [keyState, setKeyState] = useState()
   const { id } = useParams()
+
+  useEffect(() => {
+    const fetchData = async (id?: string) => {
+      const result = await getKeyData(id)
+      setKeyState(result)
+    }
+    fetchData(id).catch(console.error)
+  }, [id])
 
   return <></>
 }
